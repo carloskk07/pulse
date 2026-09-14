040beba49ff1becf22be67ea693fb02c94973d5f
cursomoderno <carlosfreitaskk@gmail.com>
2026-09-14 00:40:34 -0300

    refactor: point legacy Brand to Pulsercuit

--- components/brand.tsx
+++ components/brand.tsx
@@ -1,12 +1,5 @@
-import Link from "next/link";
+import { PulsercuitBrand } from "./pulsercuit-brand";
 
 export function Brand({ compact = false }: { compact?: boolean }) {
-  return (
-    <Link className="brand" href="/" aria-label="Reward Pulse home">
-      <span className="brand-mark" aria-hidden="true">
-        <span />
-      </span>
-      {!compact && <span className="brand-word">Reward Pulse</span>}
-    </Link>
-  );
+  return <PulsercuitBrand compact={compact} />;
 }
